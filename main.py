a421892f00e1477428a40bec5c255ef575ef221e
Mikael Törni <mikaeltorni25@gmail.com>
2025-10-29 00:17:33 +0200

    Remove the image part

--- main.py
+++ main.py
@@ -27,12 +27,6 @@ completion = client.chat.completions.create(
         {
           "type": "text",
           "text": "What is in this image?"
-        },
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
-          }
         }
       ]
     }
